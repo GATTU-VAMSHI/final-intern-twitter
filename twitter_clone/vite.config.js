31b2ad6759fd0b0da662ff7c853909034cc79d72
GATTU-VAMSHI <gattuvamshi17@email.com>
2025-04-02 21:12:08 +0530

    Updated feature X or fixed bug Y

--- twitter_clone/vite.config.js
+++ twitter_clone/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure this is set to your desired output directory
+    rollupOptions: {
+      input: {
+        main: 'src/main.jsx', // Entry point for your application
+      },
+      external: ['server/*'], // Exclude server files from the bundle
+    },
+  },
 })
